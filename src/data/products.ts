bb9ca1ff912293beaac27eedf56fdcb476c4bf4b
reivenplusgit <reivenplus@gmail.com>
2026-02-05 23:59:41 +0800

    Data Added

---
+++ src/data/products.ts
@@ -0,0 +1,57 @@
+export interface Product {
+    id: string;
+    name: string;
+    price: number;
+    image: any;
+}
+
+export const PRODUCTS: Product[] = [
+    { 
+        id: '1', 
+        name: 'Pop! Anduin Wrynn', 
+        price: 14.99, 
+        image: require('../assets/images/pop-anduin.png'),
+    },
+    { 
+        id: '2', 
+        name: 'Pop! Illidan Stormrage', 
+        price: 14.99, 
+        image: require('../assets/images/pop-illidan.png'),
+    },
+    { 
+        id: '3', 
+        name: 'Pop! Jaina Proudmoore', 
+        price: 14.99, 
+        image: require('../assets/images/pop-jaina.png'),
+    },
+    { 
+        id: '4', 
+        name: 'Pop! Xalatath', 
+        price: 14.99, 
+        image: require('../assets/images/pop-xalatath.png'),
+    },
+    { 
+        id: '5', 
+        name: 'Pop! Aleria Windrunner', 
+        price: 14.99, 
+        image: require('../assets/images/pop-aleria.png'),
+    },
+    { 
+        id: '6', 
+        name: 'Pop! Thrall', 
+        price: 14.99, 
+        image: require('../assets/images/pop-thrall.png'),
+    },
+    { 
+        id: '7', 
+        name: 'Pop! Sylvanas Windrunner', 
+        price: 14.99, 
+        image: require('../assets/images/pop-sylvanas.png'),
+    },
+    { 
+        id: '1', 
+        name: 'Pop! The Lich King', 
+        price: 14.99, 
+        image: require('../assets/images/pop-lichking.png'),
+    },
+];
